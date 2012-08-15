dc84ec56920b3daad129231d5ec4596661c2ee1d
Maxim Chernyak <max@bitsonnet.com>
2012-08-15 14:22:22 -0400

    Remove self from BlockDevice methods.

--- libraries/block_device.rb
+++ libraries/block_device.rb
@@ -1,5 +1,7 @@
 module BlockDevice
-  def self.wait_for(device)
+  extend self
+
+  def wait_for(device)
     loop do
       if File.blockdev?(device)
         Chef::Log.info("device #{device} ready")
@@ -11,7 +13,7 @@ module BlockDevice
     end
   end
 
-  def self.wait_for_logical_volumes
+  def wait_for_logical_volumes
     loop do
       lvscan = `lvscan`
       if lvscan.lines.all?{|line| line.include?('ACTIVE')}
@@ -24,7 +26,7 @@ module BlockDevice
     end
   end
 
-  def self.existing_raid_at?(device)
+  def existing_raid_at?(device)
     raids = `mdadm --examine --scan`
     if raids.match(device) || raids.match(device.gsub(/md/, "md/"))
       Chef::Log.debug("Checking for existing RAID arrays at #{device}: #{raids}")
@@ -37,7 +39,7 @@ module BlockDevice
     end
   end
 
-  def self.assembled_raid_at?(device)
+  def assembled_raid_at?(device)
     raids = `mdadm --detail --scan`
     if raids.match(device)
       Chef::Log.debug("Checking for running RAID arrays at #{device}: #{raids}")
@@ -50,30 +52,30 @@ module BlockDevice
     end
   end
 
-  def self.assemble_raid(raid_device, options)
+  def assemble_raid(raid_device, options)
     Chef::Log.info "Resuming existing RAID array #{raid_device} with #{options[:disks].size} disks, RAID level #{options[:raid_level]} at #{options[:mount_point]}"
     exec_command("mdadm --assemble --verbose #{raid_device} #{options[:disks].join(' ')}") or raise "Failed to assemble the RAID array at #{raid_device}"
   end
 
-  def self.create_raid(raid_device, options)
+  def create_raid(raid_device, options)
     Chef::Log.info "creating RAID array #{raid_device} with #{options[:disks].size} disks, RAID level #{options[:raid_level]} at #{options[:mount_point]}"
     exec_command("yes n | mdadm --create --chunk=#{options[:chunk_size]} --metadata=1.2 --verbose #{raid_device} --level=#{options[:raid_level]} --raid-devices=#{options[:disks].size} #{options[:disks].join(' ')}") or raise "Failed to create the RAID array at #{raid_device}"
   end
 
-  def self.set_read_ahead(raid_device, ahead_option)
+  def set_read_ahead(raid_device, ahead_option)
     Chef::Log.info "Setting read ahead options for RAID array #{raid_device} to #{ahead_option}"
     exec_command("blockdev --setra #{ahead_option} #{raid_device}") or raise "Failed to set read ahead options for RAID array at #{raid_device} to #{ahead_option}"
   end
 
-  def self.lvm_device(raid_device)
+  def lvm_device(raid_device)
     "/dev/#{lvm_volume_group(raid_device)}/lvm#{raid_device.match(/\d+/)[0]}"
   end
 
-  def self.lvm_volume_group(raid_device)
+  def lvm_volume_group(raid_device)
     "lvm-raid-#{raid_device.match(/\d+/)[0]}"
   end
 
-  def self.existing_lvm_at?(lvm_device)
+  def existing_lvm_at?(lvm_device)
     lvms = `lvscan`
     if lvms.match(lvm_device)
       Chef::Log.debug("Checking for active LVM volumes at #{lvm_device}: #{lvms}")
@@ -86,7 +88,7 @@ module BlockDevice
     end
   end
 
-  def self.create_lvm(raid_device, options)
+  def create_lvm(raid_device, options)
     Chef::Log.info "creating LVM volume out of #{raid_device} with #{options[:disks].size} disks at #{options[:mount_point]}"
     unless lvm_physical_group_exists?(raid_device)
       exec_command("pvcreate #{raid_device}") or raise "Failed to create LVM physical disk for #{raid_device}"
@@ -100,7 +102,7 @@ module BlockDevice
     end
   end
 
-  def self.lvm_physical_group_exists?(raid_device)
+  def lvm_physical_group_exists?(raid_device)
     pvscan = `pvscan`
     if pvscan.match(raid_device)
       Chef::Log.debug("Checking for existing LVM physical disk for #{raid_device}: #{pvscan}")
@@ -113,7 +115,7 @@ module BlockDevice
     end
   end
 
-  def self.lvm_volume_group_exists?(raid_device)
+  def lvm_volume_group_exists?(raid_device)
     vgscan = `vgscan`
     if vgscan.match(lvm_volume_group(raid_device))
       Chef::Log.debug("Checking for existing LVM volume group for #{lvm_volume_group(raid_device)}: #{vgscan}")
@@ -126,7 +128,7 @@ module BlockDevice
     end
   end
 
-  def self.lvm_volume_exits?(raid_device)
+  def lvm_volume_exits?(raid_device)
     wait_for_logical_volumes
     lvscan = `lvscan`
     if lvscan.match(lvm_device(raid_device))
@@ -140,7 +142,7 @@ module BlockDevice
     end
   end
 
-  def self.exec_command(command)
+  def exec_command(command)
     Chef::Log.debug("Executing: #{command}")
     output = `#{command} 2>&1`
     if $?.success?
@@ -152,7 +154,7 @@ module BlockDevice
     end
   end
 
-  def self.translate_device_names(devices, skip = 0)
+  def translate_device_names(devices, skip = 0)
     if on_kvm? && devices.size > 0
       Chef::Log.info("Running on QEMU/KVM: Starting at /dev/sdb skipping #{skip}")
       new_devices = ('b'..'z').to_a[0 + skip, devices.size].each_with_index.map {|char, index| [ devices[index], "/dev/sd#{char}" ]  }
@@ -163,7 +165,7 @@ module BlockDevice
     end
   end
 
-  def self.on_kvm?
+  def on_kvm?
     `cat /proc/cpuinfo`.match(/QEMU/)
   end
 end
